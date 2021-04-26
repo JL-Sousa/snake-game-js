42b5b35d79b73ac406a9df38155d793da3c843af
jonyelson <jonyelson20@gmail.com>
2021-04-25 20:39:43 -0400

    feat: defined motions of the snake

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ snake[0] = {
   y: 8 * box
 }
 
+let direction = 'right'
+
 function createBG() {
   context.fillStyle = '#009432'
   context.fillRect(0, 0, 16 * box, 16 * box)
@@ -20,6 +22,29 @@ function createSnake() {
   }
 }
 
-createBG()
+function startGame() {
+  createBG()
+  createSnake()
 
-createSnake()
+  let snakeX = snake[0].x;
+  let snakeX = snake[0].y;
+
+  if (direction === 'right') snakeX += box;
+  if (direction === 'left') snakeX -= box;
+  if (direction === 'up') snakeY -= box;
+  if (direction === 'down') snakeY += box;
+
+  snake.pop()
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead)
+}
+
+let game = setInterval(startGame, 100);
+
+startGame()
+
