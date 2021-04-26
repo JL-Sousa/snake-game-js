186aa3db2a3b02c9d301ac0ed954a0a697b77928
jonyelson <jonyelson20@gmail.com>
2021-04-25 20:11:54 -0400

    feat: created snake

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 let canvas = document.getElementById('snake')
 let context = canvas.getContext('2d')
 let box = 32
+let snake = []
 
-function criarBG() {
+snake[0] = {
+  x: 8 * box,
+  y: 8 * box
+}
+
+function createBG() {
   context.fillStyle = '#009432'
   context.fillRect(0, 0, 16 * box, 16 * box)
 }
 
-criarBG()
+function createSnake() {
+  for (i = 0; i < snake.length; i++) {
+    context.fillStyle = '#1B1464'
+    context.fillRect(snake[i].x, snake[i].y, box, box)
+  }
+}
+
+createBG()
+
+createSnake()
